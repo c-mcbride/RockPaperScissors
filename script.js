711149a3ddbca6443edeb6b88212a00886ffdeb9
Califf <califfmcbride@gmail.com>
2022-06-23 20:50:40 -0500

    Add rock, paper scissor computation
    Adds r,p,s computation and allow the player to pick. Also add comments.
    At this point, the game works only in the console.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+/*This program simulates rock, paper scissors with the computer vs. the user 
+From here on out I will use the abrevation r,p,s to mean rock, paper and scissors*/
+
+//Randomly generates a number between 0 and 2 and assigns them to rock,paper, scissors
 let computerNumber = Math.floor (Math.random() * 3);
+
+//A variable to store the string equivalent to r,p,s
 let computerChoice = null; 
 
 console.log(computerNumber);
@@ -14,5 +20,56 @@ else if (computerNumber == 1) {
 else {
     computerChoice = "scissor"; 
 }
+//Prints the r,p,s choice after it is assigned
+console.log("Computer choice " + computerChoice);
 
-console.log(computerChoice)
+
+//Here the player types a string for their r,p,s choice 
+let playerChoice = prompt ("Choose rock, paper or scissors")
+console.log ("Player choice " + playerChoice)
+
+//If else statements to compare the player choice with the computer's and prints the result
+if (playerChoice == "rock") {
+
+    if (computerChoice =="paper"){
+        console.log("Paper covers rock, you LOSE!");
+    }
+
+    else if(computerChoice == "rock"){
+        console.log("The computer also chose rock, tie");
+    }
+
+    else{
+        console.log("Rock smashes scissors! YOU WIN!");
+    }
+}
+
+else if (playerChoice == "paper") {
+
+    if (computerChoice == "paper"){
+        console.log("The computer also picked paper, tie.")
+    }
+
+    else if (computerChoice == "rock"){
+        console.log("Paper covers rock! YOU WIN!");
+    }
+
+    else{
+        console.log("Scissors cut paper. YOU LOSE!");
+    }
+}
+
+else if (playerChoice == "scissors") {
+
+    if (computerChoice == "paper"){
+        console.log("Scissors cut paper. YOU WIN!");
+    }
+
+    if (computerChoice == "rock") {
+        console.log("Rock smashes scissors. YOU LOSE!");
+    }
+
+    if (computerChoice == "scissors") {
+        console.log("The computer also picked scissors. tie.");
+    }
+}
